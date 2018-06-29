9148112a1904a0feec1d08cefcfbecd02ee1ca9d
Dilson Pereira <dilsonlucas@gmail.com>
2018-06-29 17:32:00 -0300

    Added comment explaining what is returned

--- Christofides.h
+++ Christofides.h
@@ -2,6 +2,12 @@
 #include "./Matching/Matching.h"
 #include "./Matching/Graph.h"
 
+/*
+Christofides algorithm
+returns a pair containing a vector and a double
+the vector contains the indices of the edges in the solution
+the double is the solution cost
+*/
 pair< vector<int>, double > Christofides(const Graph & G, const vector<double> & cost)
 {
 	//Solve minimum spanning tree problem
